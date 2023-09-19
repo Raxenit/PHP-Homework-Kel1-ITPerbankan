3c07396127e18abb2b771ae002bbc45306339bf6
danangwidianto <2011501935@student.budiluhur.ac.id>
2023-09-19 16:54:42 +0700

    api transfer dan db transfer

--- api_transfer/transfer.php
+++ api_transfer/transfer.php
@@ -1,14 +1,11 @@
 <?php
-// Konfigurasi koneksi database
 $servername = "localhost";
-$username = "root"; // Ganti dengan username MySQL Anda
-$password = ""; // Ganti dengan password MySQL Anda
-$database = "db_transfer"; // Ganti dengan nama database Anda
+$username = "root";
+$password = ""; 
+$database = "db_transfer"; 
 
-// Membuat koneksi ke database
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Memeriksa koneksi
 if ($conn->connect_error) {
     die("Koneksi ke database gagal: " . $conn->connect_error);
 }
@@ -21,7 +18,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $to_account = $data['to_account'];
         $amount = $data['amount'];
 
-        // Memeriksa saldo akun pengirim
         $sql = "SELECT balance FROM accounts WHERE account_number = '$from_account'";
         $result = $conn->query($sql);
 
@@ -30,15 +26,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $current_balance = $row['balance'];
 
             if ($current_balance >= $amount && $amount > 0) {
-                // Melakukan transfer
                 $new_balance_from = $current_balance - $amount;
                 $new_balance_to = $amount;
 
-                // Menyimpan transaksi
                 $sql = "INSERT INTO transactions (from_account_number, to_account_number, amount) VALUES ('$from_account', '$to_account', $amount)";
                 $conn->query($sql);
 
-                // Memperbarui saldo akun
                 $sql = "UPDATE accounts SET balance = $new_balance_from WHERE account_number = '$from_account'";
                 $conn->query($sql);
 
@@ -60,6 +53,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 
-// Menutup koneksi ke database
 $conn->close();
 ?>
